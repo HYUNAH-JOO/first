72f76392690f97fd702dc17348282d9f4c6dd2d7
HYUNAH-JOO <zz4231@naver.com>
2019-04-02 21:23:05 +0900

    first commit

---
+++ hello.py
@@ -0,0 +1,58 @@
+print("helloworld")
+
+a = "hello"
+print(a)
+print(type(10))
+print(type("10"))
+print(type(1.5))
+
+x = 37
+y = 27
+print(x+y)
+print(x*y)
+print(x/y)
+print(x//y)
+print(x%y)
+
+print("\"\"")
+
+print("\n")
+
+# print("""안녕하세요 지금
+# 엄청
+#     배가    고파요""")
+
+print("""안녕하세요 지금
+엄청
+    배가    고파요""")
+
+h = "치킨에는"
+p = "맥주지"
+
+# print("\n")
+
+# print(h+p)
+# print(h*3)
+
+print("\n")
+
+print(h+p)
+print(h*3)
+
+#print("="*20)
+
+print("="*20)
+
+u = input(h)
+
+
+m = "안녕하세요"
+print(m[3])
+print(m[0:3])
+print(m[0:5])
+print(m[:])
+print(m[-1])
+print(m[-5])
+
+print(len(m))
+print(m.count("안"))
